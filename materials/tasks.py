baf11b3644b72c54f29a70206521447cf09bde8a
neymar <igor_baidikov@mail.ru>
2024-03-24 22:41:06 +0300

    26.2 fix

--- materials/tasks.py
+++ materials/tasks.py
@@ -12,13 +12,13 @@ def send_mail_about_update(course_id):
     We have to send mail after any change of course or lessons
     So we have to send mail too after creating new lesson, because this is change of course"""
     subs = Subscription.objects.filter(course_id=course_id)
+    connection = smtplib.SMTP('smtp.gmail.com')
+    connection.starttls()
+    connection.login(user=EMAIL_HOST_USER, password=EMAIL_HOST_PASSWORD)
     for sub in subs:
         message = MIMEMultipart()
         message['Subject'] = "Updating service"
         message.attach(MIMEText(f"We have to say you about updating materials of course: '{sub.course.title}'",
                                 'plain'))
-        connection = smtplib.SMTP('smtp.gmail.com')
-        connection.starttls()
-        connection.login(user=EMAIL_HOST_USER, password=EMAIL_HOST_PASSWORD)
         connection.sendmail(from_addr=EMAIL_HOST_USER, to_addrs=sub.user.email, msg=message.as_string())
-        connection.close()
+    connection.close()
